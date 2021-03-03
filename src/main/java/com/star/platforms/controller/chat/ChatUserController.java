bf5b662e1fbfea209dccf34c85e0e6ff6f2c4c8a
sam <793514513@qq.com>
2021-03-03 23:28:11 +0800

    开始即时通讯

---
+++ src/main/java/com/star/platforms/controller/chat/ChatUserController.java
@@ -0,0 +1,13 @@
+package com.star.platforms.controller.chat;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sam
+ */
+@RestController
+@RequestMapping("/chat/user")
+public class ChatUserController {
+
+}
